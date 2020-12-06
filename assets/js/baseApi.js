ab24bd5862617383fd408acc665da6686ec0c024
john <576377303@qq.com>
2020-12-06 20:42:11 +0800

    添加首页

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -2,5 +2,20 @@ $(function(){
     $.ajaxPrefilter(function(options) {
         // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
         options.url = 'http://ajax.frontend.itheima.net' + options.url
+        //统一为有权限的接口设置`headers`请求头
+        if(options.url.includes('/my/')){
+          options.headers = {
+            Authorization:localStorage.getItem('token') || ''
+          }
+        }
+        //控制用户的访问权限
+        options.complete = function(res) {
+          console.log(res);
+          // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+          if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+          }
+        }
       })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+$(function(){
+    Getuserinfo()
+
+//退出功能
+    $('#escBtn').on('click',function(){
+        layui.layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            localStorage.removeItem('token')
+            location.href = 'login.html'
+            
+            layer.close(index);
+          });
+    })
+
+
+})
+
+//获取用户信息
+function Getuserinfo(){
+$.get('/my/userinfo',function(res){
+    if(res.status !== 0) return layui.layer.msg('获取用户信息失败！')
+    Render(res.data)
+})
+}
+
+//渲染信息
+function Render(user){
+    //设置名字
+    let uname = user.nickname || user.username
+    $('.welcome').html('欢迎&nbsp;&nbsp;'+uname)
+    //设置头像
+    if(user.user_pic){
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-img').hide()
+    }else{
+        $('.layui-nav-img').hide()
+        $('.text-img').html(uname[0].toUpperCase()).show()
+    }
+}
